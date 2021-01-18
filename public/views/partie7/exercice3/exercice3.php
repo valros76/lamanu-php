ba19623460c6f45a6184b97daac0281e13d34845
valros76 <valerian.dufrene@gmail.com>
2021-01-18 14:05:56 +0100

    Add exercice 3 part 7

--- public/views/partie7/exercice3/exercice3.php
+++ public/views/partie7/exercice3/exercice3.php
@@ -2,6 +2,15 @@
 include $root . "public/includes/partie7/include_exercice3.php";
 $pageTitle = "Exercice 3 - P7 - PHP";
 $headTitle = "Exercice 3 - P7";
+if(isset($_POST) && !empty($_POST)){
+   $username = isset($_POST["username"]) && !empty($_POST["username"]) ? $_POST["username"] : "";
+   $password = isset($_POST["password"]) && !empty($_POST["password"]) ? password_hash($_POST["password"], PASSWORD_DEFAULT) : "";
+   if(!empty($username) && !empty($password)){
+      $_COOKIES["username"] = $username;
+      $_COOKIES["password"] = $password;
+      //NE JAMAIS REPRODUIRE LA LIGNE DU DESSUS !!! 😱
+   }
+}
 ob_start();; ?>
 
 <section class="main-sections">
@@ -9,15 +18,24 @@ ob_start();; ?>
       Consigne
    </h2>
    <p class="main-sections-description">
-      Avec le tableau de l'exercice 1, afficher la valeur de l'index 5.
+      Faire un formulaire qui permet de récupérer le login et le mot de passe de l'utilisateur.<br/> 
+      A la validation du formulaire, stocker les informations dans un cookie.
    </p>
    <article class="main-articles">
       <h3 class="main-articles-title">
          Rendu visuel
       </h3>
-      <?php
-      echo "<p>" . $months[5] . "</p>";
-      ?>
+      <form action="<?= $_SERVER["REQUEST_URI"];?>" method="POST">
+         <label for="username">
+            Pseudo
+         </label>
+         <input type="text" placeholder="user42" name="username" id="username" required />
+         <label for="password">
+            Mot de passe
+         </label>
+         <input type="password" placeholder="********" name="password" id="password" required />
+         <input type="submit" value="Se connecter">
+      </form>
    </article>
 </section>
 
