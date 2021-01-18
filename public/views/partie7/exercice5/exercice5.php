6f26fc2d4768741a6c02552a2271e5fce30dc697
valros76 <valerian.dufrene@gmail.com>
2021-01-18 14:34:46 +0100

    Add exercice 5 part 7

--- public/views/partie7/exercice5/exercice5.php
+++ public/views/partie7/exercice5/exercice5.php
@@ -1,7 +1,23 @@
 <?php
 include $root . "public/includes/partie7/include_exercice5.php";
-$pageTitle = "Exercice 7 - P6 - PHP";
-$headTitle = "Exercice 7 - P6";
+$pageTitle = "Exercice 5 - P7 - PHP";
+$headTitle = "Exercice 5 - P7";
+if (isset($_POST) && !empty($_POST)) {
+   $username = isset($_POST["username"]) && !empty($_POST["username"]) ? $_POST["username"] : "";
+   $password = isset($_POST["password"]) && !empty($_POST["password"]) ? password_hash($_POST["password"], PASSWORD_DEFAULT) : "";
+   // if (isset($_COOKIE["username"]) && isset($_COOKIE["password"])) {
+   //    unset($_COOKIE["username"]);
+   //    unset($_COOKIE["password"]);
+   // }
+   if (!isset($_COOKIE["username"]) && !empty($username) && !isset($_COOKIE["password"]) && !empty($password)) {
+      setcookie("username", $username);
+      setcookie("password", $password);
+      //NE JAMAIS REPRODUIRE LA LIGNE DU DESSUS !!! 😱
+   } elseif (isset($_COOKIE["username"]) && !empty($username) && isset($_COOKIE["password"]) && !empty($password)) {
+      $_COOKIE["username"] = $username;
+      $_COOKIE["password"] = $password;
+   }
+}
 ob_start();; ?>
 
 <section class="main-sections">
@@ -9,18 +25,39 @@ ob_start();; ?>
       Consigne
    </h2>
    <p class="main-sections-description">
-      Créer un tableau associatif avec comme index le numéro des départements des Hauts de France et en valeur leur nom.
+      Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiche.
    </p>
    <article class="main-articles">
       <h3 class="main-articles-title">
          Rendu visuel
       </h3>
+      <form action="<?= $_SERVER["REQUEST_URI"]; ?>" method="POST">
+         <label for="username">
+            Pseudo
+         </label>
+         <input type="text" placeholder="user42" name="username" id="username" required />
+         <label for="password">
+            Mot de passe
+         </label>
+         <input type="password" placeholder="********" name="password" id="password" required />
+         <input type="submit" value="Se connecter">
+      </form>
       <?php
-      var_dump($departements);; ?>
+      if (empty($_COOKIE["username"]) && empty($_COOKIE["password"])) {; ?>
+         <p>
+            Les cookies sont vides !<br />
+            Vous pourrez soumettre à nouveau le formulaire pour modifier les cookies.
+         </p>
+      <?php } else {; ?>
+         <p>
+            Votre pseudo : <?= $_COOKIE["username"]; ?><br />
+            Votre mot de passe : <?= $_COOKIE["password"]; ?>
+         </p>
+      <?php
+      }; ?>
    </article>
 </section>
 
 <?php
 $mainContent = ob_get_clean();
-$scripts = "<script src='public/sources/js/spoiler.js'></script>";
 require_once $root . "/public/templates/default_template.php";; ?>
