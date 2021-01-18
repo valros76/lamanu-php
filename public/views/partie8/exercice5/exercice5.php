723badd0cd3e7e9fe8ff45c9b42142e0603ba341
valros76 <valerian.dufrene@gmail.com>
2021-01-18 15:40:18 +0100

    Add exercice 5 part 8

--- public/views/partie8/exercice5/exercice5.php
+++ public/views/partie8/exercice5/exercice5.php
@@ -2,22 +2,10 @@
 include $root . "public/includes/partie8/include_exercice5.php";
 $pageTitle = "Exercice 5 - P8 - PHP";
 $headTitle = "Exercice 5 - P8";
-if (isset($_POST) && !empty($_POST)) {
-   $username = isset($_POST["username"]) && !empty($_POST["username"]) ? $_POST["username"] : "";
-   $password = isset($_POST["password"]) && !empty($_POST["password"]) ? password_hash($_POST["password"], PASSWORD_DEFAULT) : "";
-   // if (isset($_COOKIE["username"]) && isset($_COOKIE["password"])) {
-   //    unset($_COOKIE["username"]);
-   //    unset($_COOKIE["password"]);
-   // }
-   if (!isset($_COOKIE["username"]) && !empty($username) && !isset($_COOKIE["password"]) && !empty($password)) {
-      setcookie("username", $username);
-      setcookie("password", $password);
-      //NE JAMAIS REPRODUIRE LA LIGNE DU DESSUS !!! 😱
-   } elseif (isset($_COOKIE["username"]) && !empty($username) && isset($_COOKIE["password"]) && !empty($password)) {
-      $_COOKIE["username"] = $username;
-      $_COOKIE["password"] = $password;
-   }
-}
+setlocale(LC_TIME, "fr");
+$timestamp_fr = time();
+$timestamp_2016 = strtotime("16 May 2016");
+$delay = intval(($timestamp_fr - $timestamp_2016) / 86400);
 ob_start();; ?>
 
 <section class="main-sections">
@@ -25,36 +13,15 @@ ob_start();; ?>
       Consigne
    </h2>
    <p class="main-sections-description">
-      Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiche.
+      Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016.
    </p>
    <article class="main-articles">
       <h3 class="main-articles-title">
          Rendu visuel
       </h3>
-      <form action="<?= $_SERVER["REQUEST_URI"]; ?>" method="POST">
-         <label for="username">
-            Pseudo
-         </label>
-         <input type="text" placeholder="user42" name="username" id="username" required />
-         <label for="password">
-            Mot de passe
-         </label>
-         <input type="password" placeholder="********" name="password" id="password" required />
-         <input type="submit" value="Se connecter">
-      </form>
-      <?php
-      if (empty($_COOKIE["username"]) && empty($_COOKIE["password"])) {; ?>
-         <p>
-            Les cookies sont vides !<br />
-            Vous pourrez soumettre à nouveau le formulaire pour modifier les cookies.
-         </p>
-      <?php } else {; ?>
-         <p>
-            Votre pseudo : <?= $_COOKIE["username"]; ?><br />
-            Votre mot de passe : <?= $_COOKIE["password"]; ?>
-         </p>
-      <?php
-      }; ?>
+      <p>
+         Nombre de jours depuis le 16 Mai 2016 : <?= $delay; ?>
+      </p>
    </article>
 </section>
 
