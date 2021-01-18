7d576c8348fcb86d06c1cc39c25da93b5b26a0ae
valros76 <valerian.dufrene@gmail.com>
2021-01-18 14:56:57 +0100

    Add exercice 3 p 8

--- public/views/partie8/exercice2/exercice2.php
+++ public/views/partie8/exercice2/exercice2.php
@@ -1,13 +1,8 @@
 <?php
-if(session_status() == PHP_SESSION_NONE){
-   session_start();
-}
 include $root . "public/includes/partie8/include_exercice2.php";
 $pageTitle = "Exercice 2 - P8 - PHP";
 $headTitle = "Exercice 2 - P8";
-$_SESSION["lastname"] = "Concepteur";
-$_SESSION["firstname"] = "Webdevoo";
-$_SESSION["age"] = "25";
+$date_fr = date("d-m-Y");
 ob_start();; ?>
 
 <section class="main-sections">
@@ -15,18 +10,14 @@ ob_start();; ?>
       Consigne
    </h2>
    <p class="main-sections-description">
-      Sur la page index, faire un lien vers une autre page.<br/>
-       Passer d'une page à l'autre le contenu des variables lastname, firstname et age grâce aux sessions.<br/>
-      Ces variables auront été définies directement dans le code.<br/>
-      Il faudra afficher le contenu de ces variables sur la deuxième page.
+      Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16).
    </p>
    <article class="main-articles">
       <h3 class="main-articles-title">
          Rendu visuel
       </h3>
       <p>
-         Passage à la page 2 avec données stockées en session.<br/>
-         <a href="?part=partie7&page=exercice2&otherpage=page2">Aller sur la page 2</a>
+         Date du jour : <?= $date_fr; ?>
       </p>
    </article>
 </section>
--- public/views/partie8/exercice3/exercice3.php
+++ public/views/partie8/exercice3/exercice3.php
@@ -2,16 +2,10 @@
 include $root . "public/includes/partie8/include_exercice3.php";
 $pageTitle = "Exercice 3 - P8 - PHP";
 $headTitle = "Exercice 3 - P8";
-if(isset($_POST) && !empty($_POST)){
-   $username = isset($_POST["username"]) && !empty($_POST["username"]) ? $_POST["username"] : "";
-   $password = isset($_POST["password"]) && !empty($_POST["password"]) ? password_hash($_POST["password"], PASSWORD_DEFAULT) : "";
-   if(!empty($username) && !empty($password)){
-      setcookie("username", $username, time()+60);
-      setcookie("password", $password, time()+60);
-      //NE JAMAIS REPRODUIRE LA LIGNE DU DESSUS !!! 😱
-      //Cookie time : 60 secondes
-   }
-}
+setlocale(LC_TIME, "de");
+$date_de = strftime("%A %e %B %G");
+setlocale(LC_TIME, "fr");
+$date_fr = strftime("%A %e %B %G");
 ob_start();; ?>
 
 <section class="main-sections">
@@ -19,24 +13,16 @@ ob_start();; ?>
       Consigne
    </h2>
    <p class="main-sections-description">
-      Faire un formulaire qui permet de récupérer le login et le mot de passe de l'utilisateur.<br/> 
-      A la validation du formulaire, stocker les informations dans un cookie.
+      Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16).
    </p>
    <article class="main-articles">
       <h3 class="main-articles-title">
          Rendu visuel
       </h3>
-      <form action="<?= $_SERVER["REQUEST_URI"];?>" method="POST">
-         <label for="username">
-            Pseudo
-         </label>
-         <input type="text" placeholder="user42" name="username" id="username" required />
-         <label for="password">
-            Mot de passe
-         </label>
-         <input type="password" placeholder="********" name="password" id="password" required />
-         <input type="submit" value="Se connecter">
-      </form>
+      <p>
+         Date du jour (DE) : <?= $date_de; ?><br/>
+         Date du jour (FR) : <?= $date_fr; ?>
+      </p>
    </article>
 </section>
 
